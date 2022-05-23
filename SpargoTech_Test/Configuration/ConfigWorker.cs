48a0197d7298525ae6d499abcd6bfd773d7d1809
NickGulyaev <106115399+NickGulyaev@users.noreply.github.com>
2022-05-24 02:16:06 +0500

    Add files via upload

---
+++ SpargoTech_Test/Configuration/ConfigWorker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpargoTech_Test.Configuration
+{
+    public static class ConfigWorker
+    {
+        public static string GetConnectionString()
+        {
+            try
+            {              
+                return ConfigurationManager.ConnectionStrings["ConnectionString1"].ToString();
+            }
+            catch
+            {              
+                return "";
+            }
+        }
+    }
+}
+
+
